df56070bd4d87aa632876f07e2e9c5072e2ca4d2
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-03 13:46:44 +0300

    start main page

---
+++ client/src/components/categoriesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const CategoriesPage = () => {
+    return (
+        <div>
+            <h1> Categories Page </h1>
+            <h3>Фрукти</h3>
+            <h3>Овочі</h3>
+            <h3>Зелень</h3>
+        </div>
+    )
+}
+
+
