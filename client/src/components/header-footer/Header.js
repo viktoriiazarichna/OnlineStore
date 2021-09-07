4cde731c353c09afb71cee28a7d488d3b33be16c
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-07 10:37:54 +0300

    header css

--- client/src/components/header-footer/Header.js
+++ client/src/components/header-footer/Header.js
@@ -5,15 +5,13 @@ import './Header-Footer.css';
 export default function Header() {
   return (
     <header>
-      <span> menu </span>
-      <div className="headerBlock">
-        <div className={'headerLinkBlock'}>
-          <h1>
-            <Link to={'/'} className={'headerLink', 'mainTitle'}> Еко-лавка </Link>
-          </h1>
-          <Link to={'/login'} className={'headerLink'}> Увійти </Link>
-        </div>
-        <span>cart:0</span>
+      <span id={'menu'}> menu </span>
+      <h1 id={'mainTitle'}>
+        <Link to={'/'} className={'headerLink'}> Еко-лавка </Link>
+      </h1>
+      <div>
+        <Link to={'/login'} className={'headerLink'}> Увійти </Link>
+        <span id={'cart'}>cart:0</span>
       </div>
     </header>
   )
