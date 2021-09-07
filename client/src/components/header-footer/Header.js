4439c07ae2c77bb29460858032616474f5557d3e
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-07 10:01:37 +0300

    router to login

--- client/src/components/header-footer/Header.js
+++ client/src/components/header-footer/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header-Footer.css';
 
 export default function Header() {
   return (
     <header>
-      <div> menu </div>
-      <div>
-        <Link to={'/'}> Головна </Link>
-        <Link to={'/login'}> Увійти </Link>
+      <span> menu </span>
+      <div className="headerBlock">
+        <div className={'headerLinkBlock'}>
+          <h1>
+            <Link to={'/'} className={'headerLink', 'mainTitle'}> Еко-лавка </Link>
+          </h1>
+          <Link to={'/login'} className={'headerLink'}> Увійти </Link>
+        </div>
         <span>cart:0</span>
       </div>
     </header>
