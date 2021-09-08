58cbc89c9d3b3d1f5e45bdd31febd2913ae922e2
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-09-08 16:57:05 +0300

    categories routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 
 import { MainPage } from './components/pages';
+import { MainContextProvider } from './components/context';
 
 function App() {
   return (
-    <div>
+    <MainContextProvider>
       <MainPage />
-    </div>
+    </MainContextProvider>
   );
 }
 
--- client/src/components/pages/MainPage.js
+++ client/src/components/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './MainPage.css';
 
@@ -6,23 +6,23 @@ import { Account } from '../account';
 import { Header, Footer } from '../header-footer';
 import { Categories } from '../products';
 import { Payment, Delivery, Contacts, Rules } from "../pages";
-import { MainContextProvider } from '../context';
+import { MainContext } from '../context';
 
 export default function MainPage() {
   const [isVisible, setIsVisible] = useState(false);
+  const { categories } = useContext(MainContext);
 
   return (
     <div>
-    <MainContextProvider>
       <Router>
         <Header isVisible={isVisible} setIsVisible={setIsVisible} />
 
         <main className={isVisible ? 'transparentGrey' : ''}>
           <Switch>
             <Route path={'/login'}> <Account /> </Route>
-            <Route path={'/fruits'}> Фрукти </Route>
-            <Route path={'/vegetables'}> Овочі </Route>
-            <Route path={'/greens'}> Зелень </Route>
+            {categories.map(category => (
+              <Route path={`/${category.nameEnglish}`} key={category._id}> {category.name} </Route>
+            ))}
             <Route path={'/payment'}> <Payment/> </Route>
             <Route path={'/delivery'}> <Delivery/> </Route>
             <Route path={'/contacts'}> <Contacts/> </Route>
@@ -33,7 +33,6 @@ export default function MainPage() {
         
         <Footer isVisible={isVisible} />
       </Router>
-    </MainContextProvider>
     </div>
   )
 }
