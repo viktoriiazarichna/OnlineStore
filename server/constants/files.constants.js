05b7df68b9a741a9ac17164559212229fc5e05a4
viktoriiazarichna <76845092+viktoriiazarichna@users.noreply.github.com>
2021-12-14 21:57:23 +0200

    1

---
+++ server/constants/files.constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    IMAGE_MAX_SIZE: 2 * 1024 * 1024,
+    IMAGE_MIMETYPES: [
+        'image/gif',
+        'image/jpeg',
+        'image/jpg',
+        'image/pjpeg',
+        'image/png',
+        'image/tiff',
+        'image/webp'
+    ],
+};
